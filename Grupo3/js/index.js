f12f1d7c258fa40d9d9d40ecd28a7526fbc4d109
mecha <mechamena@gmail.com>
2018-05-08 10:22:47 -0300

    arreglos

--- Grupo3/js/index.js
+++ Grupo3/js/index.js
@@ -1,24 +1,31 @@
 function validarForm (){
  var obligatorio = $(".Obligatorio");
- var error= $('.error');
+ var error = $('.error');
+ var valido = true;
 
  for(var i=0;i<obligatorio.length; i++){
   //VALIDAR SELECTS ORIGEN Y DESTINO
    if (obligatorio[i].tagName == "SELECT") {
       if (obligatorio[i].value < 0) {
-        error[i].innerHTML= 'Campo Obligatorio'     
+        error[i].innerHTML= 'Campo Obligatorio';
+        valido = false;
       }
     }
 
   if (obligatorio[i].tagName == 'INPUT'){
     if(obligatorio[i].value.length == 0){
-        error[i].innerHTML= 'Campo Obligatorio'
-      }
+      error[i].innerHTML= 'Campo Obligatorio';
+      valido = false;
     }
   }
+  }
 
+  if (valido == true) {
   crearObjeto();
 }
+}
+
+
 
 function crearObjeto(){
   var pasaje = {
